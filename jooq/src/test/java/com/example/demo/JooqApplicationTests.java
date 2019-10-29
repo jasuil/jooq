3f9957e7f942be3c1239a2c48ed89245ea0906aa
lp90620 <lp90620@linecorp.com>
2019-10-29 15:29:59 +0900

    jooq mysql
    mysql with maven

--- jooq/src/test/java/com/example/demo/JooqApplicationTests.java
+++ jooq/src/test/java/com/example/demo/JooqApplicationTests.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import java.sql.Timestamp;
+import java.util.List;
 
 import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.impl.DSL;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,32 +11,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.genetate.tables.Posts;
-import com.example.demo.repo.PostsRepository;
+import static com.hellokoding.jooq.model.Tables.BROWSERVERIFICATION;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JooqApplicationTests {
 
 	@Autowired
-	private PostsRepository postsRepository;
+	DSLContext dslContext;
 	
 	@Test
 	@Transactional
 	public void contextLoads() {
-		
-		Result<Record> result = postsRepository.findAll();
-		
-		  for (Record r : result) {
-              String content = r.getValue(Posts.POSTS.CONTENT);
-              String title = r.getValue(Posts.POSTS.TITLE);
-              Timestamp date = r.getValue(Posts.POSTS.CREATED_ON);
-
-              System.out.println("content: " + title + " first content: " + content + " date: " +  date);
-          }
-		  
-		 int updateResult =  postsRepository.save("성일짱", "ㅎㅎㅎ");
-		 
+		List<String> list = dslContext.select(BROWSERVERIFICATION.CODE).from(BROWSERVERIFICATION).fetchInto(String.class);
+		System.out.println(list);
 	}
 
 }
